96c589402ec0d0576f80b3b340d2f2290194743b
Julio Silva <juansivas@outlook.com>
2019-11-13 05:26:58 -0600

    login done

--- MovieNow/public_html/js/signup.js
+++ MovieNow/public_html/js/signup.js
@@ -4,19 +4,24 @@
  * and open the template in the editor.
  */
 
-window.addEventListener('load', cargar);
+window.addEventListener('load', btnSignUp);
+window.addEventListener('load', btnLogin);
 var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
 var msg;
 
 db.transaction(function (tx){
-    tx.executeSql('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, email TEXT, firstName TEXT, lastName TEXT, username TEXT, password TEXT)');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS users (email TEXT, firstName TEXT, lastName TEXT, username TEXT, password TEXT)');
 });
 
-function cargar(){
-    document.getElementById('btnSave').addEventListener("click", saveUserInfo);
+function btnSignUp(){
+    document.getElementById('btnSignUp').addEventListener("click", SaveUserInfo);
 }
 
-function saveUserInfo(){
+function btnLogin(){
+    document.getElementById('btnLogin').addEventListener("click",comparUserInfo);
+}
+
+function SaveUserInfo(){
     var email = document.getElementById('email').value;
     var firstName = document.getElementById('firstname').value;
     var lastName = document.getElementById('lastname').value;
@@ -24,11 +29,35 @@ function saveUserInfo(){
     var password = document.getElementById('password').value;
     var confirmedPassword = document.getElementById('confirmpassword').value;
     
-    if(password !== confirmedPassword){
+    /*
+     * validation for email, username and password
+    db.transaction(function(tx){
+             tx.executeSql('SELECT * FROM users WHERE (email='+email+', username='+username+',password='+password+')');
+          });
+    */
+    if(email === "" || firstName === "" || lastName === "" || username === "" || password === "" || confirmedPassword === ""){
+        window.alert("You must fill every field!");
+    }else if(password !== confirmedPassword){
         window.alert("Your password is different! Try again");
     } else {
          db.transaction(function(tx){
              tx.executeSql('INSERT INTO users (email, firstName, lastName, username, password) VALUES(?,?,?,?,?)', [email, firstName, lastName, username, password]);
           });
     }
+}
+
+function comparUserInfo(){
+    var usernameLog = document.getElementById('usernameLog').value;
+    var passwordLog = document.getElementById('passwordLog').value;
+    
+    db.transaction(function(tx){
+             tx.executeSql('SELECT * FROM users WHERE username=? AND password=?',[usernameLog, passwordLog], function(tx,results) {
+                 if(results.rows.length > 0 ){
+                     window.alert("Login Succesful");
+                     window.location = "home.html";
+                 } else {
+                     window.alert("Invalid username or password");
+                 }
+             }, null);
+         });
 }
